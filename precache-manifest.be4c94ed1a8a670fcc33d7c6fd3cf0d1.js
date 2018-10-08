4d82906b3879f217f93dcf4bef66717dddad9e4c
Felipe Menezes <felipemenezes.web@gmail.com>
2018-10-07 23:53:50 -0300

    Inicialização do repositório para visualização de aplicação React simples.

---
+++ precache-manifest.be4c94ed1a8a670fcc33d7c6fd3cf0d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be4a084bb32adc87730a",
+    "url": "/crud-react-io/static/js/runtime~main.be4a084b.js"
+  },
+  {
+    "revision": "5d2b2bb353f28e3117ee",
+    "url": "/crud-react-io/static/js/main.5d2b2bb3.chunk.js"
+  },
+  {
+    "revision": "a6c91d6ff9044cc315f9",
+    "url": "/crud-react-io/static/js/1.a6c91d6f.chunk.js"
+  },
+  {
+    "revision": "5d2b2bb353f28e3117ee",
+    "url": "/crud-react-io/static/css/main.dc3a467a.chunk.css"
+  },
+  {
+    "revision": "422d4062d639ff54a08e735192057948",
+    "url": "/crud-react-io/index.html"
+  }
+];
